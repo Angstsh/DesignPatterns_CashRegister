5676ba7df2cbe8c5c9623b76609705f9e0fbe874
Angstsh <1305065108@qq.com>
2020-10-15 15:30:11 +0800

    商场收银系统v1.0

---
+++ CashRegister/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CashRegister
+{
+    public partial class Form1 : Form
+    {
+        //声明一个double变量total来计算总计
+        double total = 0.0d;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            //计算每个商品合计
+            double totalPrices = Convert.ToDouble(txtPrice.Text) * Convert.ToDouble(txtNum.Text);
+            //将每个商品合计计入总计
+            total = total + totalPrices;
+            //在列表框中显示信息
+            lbxList.Items.Add("单价："+txtPrice.Text+"数量："+txtNum.Text+"合计："+totalPrices.ToString());
+            //在lblResult标签上显示总计数
+            lblResult.Text = total.ToString();
+        }
+    }
+}
